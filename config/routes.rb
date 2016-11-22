17e3dfab35a82d929fec15f73b751a54b8009b4e
wladimir janssens <wladjanssens@gmail.com>
2016-11-22 15:20:26 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # GET '/tasks/:id/edit', to: "tasks#edit"
+  # PATCH '/tasks/:id'
+
+  # DELETE '/tasks/:id'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+
+Task.create(name: "Arnaud", task_name: "Do Laundry")
+Task.create(name: "Murielle", task_name: "Go take a coffee for the team")
+Task.create(name: "Wlad", task_name: "find new idea")
